887d9aa7cbca38d0ed0707505e741ec2e8cf9016
Bjarki Mar Gunnarsson <bmg18@hi.is>
2021-03-18 21:26:41 +0000

    uppfærði controller fyrir hrátt viðmót

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -1,4 +1,37 @@
 package sample;
 
-public class Controller {
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.ChoiceBox;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class Controller implements Initializable {
+    @FXML
+    private Button searchButton;
+    @FXML
+    private ChoiceBox afangaStadir;
+    @FXML
+    private CheckBox fimmStjornur;
+    @FXML
+    private CheckBox fjorarStjornur;
+    @FXML
+    private CheckBox trjarStrjornur;
+
+    private DataFactory dataFactory = new DataFactory();
+
+    public Controller(ChoiceBox afangaStadir) {
+        this.afangaStadir = afangaStadir;
+    }
+
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        afangaStadir.getItems().add("Reykjavík");
+        afangaStadir.getItems().add("Akureyri");
+        afangaStadir.getItems().add("Ísafjörður");
+    }
 }
