1fd9651f205c9930696f01931b11eabc221d368c
WinterSoldier16 <vsg8@hi.is>
2021-04-13 18:28:24 +0000

    Setti inn insert skipanir fyrir hótelin í SQL

--- src/sample/HotelDataBase.sql
+++ src/sample/HotelDataBase.sql
@@ -1,4 +1,4 @@
-CREATE TABLE HOTEL
+create TABLE HOTEL
 ( HotelID INT
 , HotelName VARCHAR(30)
 , HotelLocation VARCHAR(30)
@@ -11,7 +11,7 @@ CREATE TABLE HOTEL
 , HotelBasePrice INT
 );
 
-CREATE TABLE ROOM
+create TABLE ROOM
 ( RoomID INT
 , RoomCategory INT
 , RoomCapacity INT
@@ -21,7 +21,7 @@ CREATE TABLE ROOM
 , RoomHotelID INT
 );
 
-CREATE TABLE BOOKING
+create TABLE BOOKING
 ( BookingID INT
 , BookingHotelID INT
 , BookingUser VARCHAR(30)
@@ -29,20 +29,20 @@ CREATE TABLE BOOKING
 , BookingPaymentFinalized BOOLEAN
 );
 
-CREATE TABLE BOOKING_ROOM
+create TABLE BOOKING_ROOM
 ( BookingID INT
 , BookingRoomID INT
 , BookingArrDate VARCHAR(30)
 , BookingDepDate VARCHAR(30)
-)
+);
 
-CREATE TABLE USER
+create TABLE USER
 ( UserID INT
 , UserName VARCHAR(30)
 , UserEmail VARCHAR(30)
 );
 
-CREATE TABLE PAYMENT
+create TABLE PAYMENT
 ( PaymentID INT
 , PaymentStatus BOOLEAN
 , PaymentAmount REAL
@@ -51,4 +51,15 @@ CREATE TABLE PAYMENT
 , PaymentCardNumber INT
 , PaymentCardExpDate VARCHAR(30)
 , PaymentCardCVC INT
-)
+);
+
+INSERT INTO HOTEL (HotelID, HotelName, HotelLocation, HotelAddress, HotelPostCode, HotelPhoneNumber, HotelStarRating, HotelAmenities, HotelType, HotelBasePrice)
+VALUES 
+                  (1, "Economy Hotel Reykjavík", "Reykjavík", "Þórunnartún 1", 105, 5550000, 3, "BREAKFAST_INCLUDED, PARKING", 1, 10000)
+                  (2, "Comfort Hotel Reykjavík", "Reykjavík", "Mýrargata 2", 102, 5550001, 4, "SPA, FREE_WIFI", 2, 14000)
+                  (3, "Economy Hotel Egilsstaðir", "Egilsstaðir", "Lyngás 5-7", 700, 4550000, 3, "BREAKFAST_INCLUDED, HANDICAP_ACCESSIBLE", 1, 10000)
+                  (4, "Comfort Hotel Egilsstaðir", "Egilsstaðir", "Kaupvangur 17", 700, 4550001, 5, "RESTAURANT, SPA, PARKING", 2,14000)
+                  (5, "Economy Hotel Akureyri", "Akureyri", "Þingvallastræti 23", 600, 4560000, 3, "FREE_WIFI", 1, 10000)
+                  (6, "Comfort Hotel Akureyri", "Akureyri", "Hafnarstræti 67", 600, 4560001, 4, "RESTAURANT, PARKING, FREE_WIFI", 2, 14000)
+                  (7, "Economy Hotel Ísafjörður", "Ísafjörður",  "Silfurtorgi 2", 400, 4500000, 3, "BREAKFAST_INCLUDED, PARKING", 1, 10000)
+                  (8, "Comfort Hotel Ísafjörður", "Ísafjörður", "Mánagata 3", 400, 4500001, 5, "SPA, PARKING, HANDICAP_ACCESSIBLE, FREE_WIFI", 2, 23000))
