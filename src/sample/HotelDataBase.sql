c42b8a66bccbd955f2b8208885597c5b66deeb1c
Sólveig Ásta Friðriksdóttire <solveigasfr@gmail.com>
2021-04-13 21:41:16 +0000

    hotelDataBase.db DBFactory made

--- src/sample/HotelDataBase.sql
+++ src/sample/HotelDataBase.sql
@@ -1,60 +1,61 @@
-create TABLE HOTEL
-( HotelID INT
-, HotelName VARCHAR(30)
-, HotelLocation VARCHAR(30)
-, HotelAddress VARCHAR(30)
-, HotelPostCode INT
-, HotelPhoneNumber INT
-, HotelStarRating INT
-, HotelAmenities VARCHAR(30)
-, HotelType INT
-, HotelBasePrice INT
+create table HOTEL
+( HotelID int
+, HotelName varchar(30)
+, HotelLocation varchar(30)
+, HotelAddress varchar(30)
+, HotelPostCode int
+, HotelPhoneNumber int
+, HotelStarRating int
+, HotelAmenities varchar(30)
+, HotelType int
+, HotelBasePrice int
 );
 
-create TABLE ROOM
-( RoomID INT
-, RoomCategory INT
-, RoomCapacity INT
-, RoomPriceMultiplier REAL
-, RoomAmenities VARCHAR(30)
-, RoomOccupancy VARCHAR(30)
-, RoomHotelID INT
+create table ROOM
+( RoomID int
+, RoomCategory int
+, RoomCapacity int
+, RoomPriceMultiplier real
+, RoomAmenities varchar(30)
+, RoomOccupancy varchar(30)
+, RoomHotelID int
 );
 
-create TABLE BOOKING
-( BookingID INT
-, BookingHotelID INT
-, BookingUser VARCHAR(30)
-, BookingNumOfGuests INT
-, BookingPaymentFinalized BOOLEAN
+create table BOOKING
+( BookingID int
+, BookingHotelID int
+, BookingUser varchar(30)
+, BookingNumOfGuests int
+, BookingPaymentFinalized boolean
 );
 
-create TABLE BOOKING_ROOM
-( BookingID INT
-, BookingRoomID INT
-, BookingArrDate VARCHAR(30)
-, BookingDepDate VARCHAR(30)
+create table BOOKING_ROOM
+( BookingID int
+, BookingRoomID int
+, BookingArrDate varchar(30)
+, BookingDepDate varchar(30)
 );
 
-create TABLE USER
-( UserID INT
-, UserName VARCHAR(30)
-, UserEmail VARCHAR(30)
+create table USER
+( UserID int
+, UserName varchar(30)
+, UserEmail varchar(30)
 );
 
-create TABLE PAYMENT
-( PaymentID INT
-, PaymentStatus BOOLEAN
-, PaymentAmount REAL
-, PaymentDate VARCHAR(30)
-, PaymentCardHolderName VARCHAR(30)
-, PaymentCardNumber INT
-, PaymentCardExpDate VARCHAR(30)
-, PaymentCardCVC INT
+create table PAYMENT
+( PaymentID int
+, PaymentStatus boolean
+, PaymentAmount real
+, PaymentDate varchar(30)
+, PaymentCardHolderName varchar(30)
+, PaymentCardNumber int
+, PaymentCardExpDate varchar(30)
+, PaymentCardCVC int
 );
 
+/*
 INSERT INTO HOTEL (HotelID, HotelName, HotelLocation, HotelAddress, HotelPostCode, HotelPhoneNumber, HotelStarRating, HotelAmenities, HotelType, HotelBasePrice)
-VALUES 
+VALUES
                   (1, "Economy Hotel Reykjavík", "Reykjavík", "Þórunnartún 1", 105, 5550000, 3, "BREAKFAST_INCLUDED, PARKING", 1, 10000)
                   (2, "Comfort Hotel Reykjavík", "Reykjavík", "Mýrargata 2", 102, 5550001, 4, "SPA, FREE_WIFI", 2, 14000)
                   (3, "Economy Hotel Egilsstaðir", "Egilsstaðir", "Lyngás 5-7", 700, 4550000, 3, "BREAKFAST_INCLUDED, HANDICAP_ACCESSIBLE", 1, 10000)
@@ -63,3 +64,4 @@ VALUES
                   (6, "Comfort Hotel Akureyri", "Akureyri", "Hafnarstræti 67", 600, 4560001, 4, "RESTAURANT, PARKING, FREE_WIFI", 2, 14000)
                   (7, "Economy Hotel Ísafjörður", "Ísafjörður",  "Silfurtorgi 2", 400, 4500000, 3, "BREAKFAST_INCLUDED, PARKING", 1, 10000)
                   (8, "Comfort Hotel Ísafjörður", "Ísafjörður", "Mánagata 3", 400, 4500001, 5, "SPA, PARKING, HANDICAP_ACCESSIBLE, FREE_WIFI", 2, 23000))
+*/
